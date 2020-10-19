b72ba7a983ac18cdc879e62ef76048bafa28881a
yiranlll <yiran@Yirans-MacBook-Air.local>
2020-10-19 14:53:33 -0500

    plots

--- preproc.R
+++ preproc.R
@@ -1,2 +1,11 @@
-library(dplyr)
-data = read.csv("tiantan.csv")
+library(tidyverse)
+library(lubridate)
+data = read.csv("tiantan.csv") %>% 
+  fill(PM2.5, PM10, SO2, NO2, CO, O3, TEMP, PRES, DEWP, RAIN, wd, WSPM, .direction = "up") %>%
+  mutate(date = make_date(year, month, day))
+
+# some plots
+ggplot(data, aes(x = date, y = PM2.5)) + geom_line() + facet_wrap(~year) # facets by year
+ggplot(data, aes(x = date, y = PM10)) + geom_line() + geom_smooth() # lines with smoothing
+boxplot(PM2.5 ~ year, data) # boxplot, but not very good comparison
+boxplot(PM10 ~ year, data)
